0279152dce2b1e43d3f5a698e292cedf8b4f44c9
Pap Tamas <paptamas@gmail.com>
2013-07-25 20:20:35 +0300

    Create the view file for the password recovery email.

---
+++ views/auth/email/en-us/recover.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Recover your password</title>
+<body>
+<p>Hello <?php echo $name; ?>,</p>
+<p>You have requested to reset your password for<br />
+App name at <?php echo URL::base(); ?></p>
+<p>Email:    <?php echo $email; ?></p>
+<p>Copy this link into your browser to reset your password:</p>
+<p><?php echo $url; ?></p>
+<p>If you did not request to reset your password, please disregard this email.</p>
+</body>
+</html>
