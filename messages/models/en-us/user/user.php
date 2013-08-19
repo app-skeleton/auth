a983ae52b1f83268c06c45d5d904fdfc040b4728
Pap Tamas <paptamas@gmail.com>
2013-08-19 15:32:22 +0300

    Add an error messae for the timezone validation error.

--- messages/models/en-us/user/user.php
+++ messages/models/en-us/user/user.php
@@ -7,6 +7,10 @@ array(
 
     'full_name'  => array(
         'not_empty' => 'Enter your full name.'
+    ),
+
+    'timezone'  => array(
+        'valid_timezone' => 'Please select a timezone.'
     )
 );
 ?>
