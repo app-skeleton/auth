1567656f18ac7b44539ef13da0d0c7242f42d5b6
Pap Tamas <paptamas@gmail.com>
2013-09-14 20:55:23 +0300

    Update the view for the password recovery email.

--- views/auth/email/en-us/recover.php
+++ views/auth/email/en-us/recover.php
@@ -5,10 +5,10 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Recover your password</title>
 <body>
-<p>Hello <?php echo $name; ?>,</p>
+<p>Hello <?php echo $user['first_name']; ?>,</p>
 <p>You have requested to reset your password for<br />
 <?= APPNAME; ?> at <a href="<?php echo URL::base(); ?>"><?php echo URL::base(); ?></a></p>
-<p>Email:    <?php echo $email; ?></p>
+<p>Email:    <?php echo $user['email']; ?></p>
 <p>Copy this link into your browser to reset your password:</p>
 <p><a href="<?php echo $url; ?>"><?php echo $url; ?></a></p>
 <p>If you did not request to reset your password, please disregard this email.</p>
