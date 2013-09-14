d6d65238113f4c6de47c4902e2938f03e7e0fbd5
Pap Tamas <paptamas@gmail.com>
2013-09-14 20:10:32 +0300

    Create message file for Password Recovery Link validation.

---
+++ messages/models/en-us/auth/password_recovery_link.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+return
+array(
+    'email'  => array(
+        'not_empty' => 'Provide your email address.',
+        'email' => 'Provide a valid email address.',
+        'email_exists' => 'We could not find anyone with that email.'
+    )
+);
+
